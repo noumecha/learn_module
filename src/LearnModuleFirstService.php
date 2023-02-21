d4584c0a6c89d177bbde1a5168963e0d2c555ca9
noumecha spaker <noumechaivan@gmail.com>
2023-02-21 17:12:46 +0100

    maj

--- src/LearnModuleFirstService.php
+++ src/LearnModuleFirstService.php
@@ -3,6 +3,7 @@
 namespace Drupal\learn_module;
 
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
  * Prepares the salutation to the world with service.
@@ -11,23 +12,47 @@ class LearnModuleFirstService
 {
     use StringTranslationTrait;
     /**
+     * @var \Drupal\Core\Config\ConfigFactoryInterface
+     */
+    protected $configFactory;
+
+    /**
+     * LearnModuleFirstService constructor
+     * 
+     * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+     */
+
+    public function __construct(ConfigFactoryInterface $config_factory)
+    {
+        $this->configFactory = $config_factory;
+    }
+    
+    /**
      * Returns the salutation
      */
     public function getSalutation() {
         $time = new \DateTime();
         //$user = \Drupal::currentUser();
         $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+        $config = $this->configFactory->get('hello_world.custom_salutation');
+        $salutation = $config->get('salutation');
+        //var_dump($salutation);
+        /*if ($salutation !== '' && $salutation)
+        {
+            //return $salutation;
+            return $this->t('<center> '.$salutation.' dear '.$user->get('name')->value.'</center>');
+        }*/
         // morning
         if ((int) $time->format('G') >= 00 && (int) $time->format('G') < 12) {
-            return $this->t('<center> Good morning '.$user->get('name')->value.'</center>');
+            return $this->t('<center> Good morning dear'.$user->get('name')->value.'</center>');
         }
         // afternoon
         if ((int) $time->format('G') > 12 && (int) $time->format('G') < 18) {
-            return $this->t('<center> Good afternoon '.$user->get('name')->value.'</center>');
+            return $this->t('<center> Good afternoon dear '.$user->get('name')->value.'</center>');
         }
         // evening
         if ((int) $time->format('G') >= 18) {
-            return $this->t('<center> Good evening '.$user->get('name')->value.'</center>');
+            return $this->t('<center> Good evening dear '.$salutation.' '.$user->get('name')->value.'</center>');
         }
     }
 }
