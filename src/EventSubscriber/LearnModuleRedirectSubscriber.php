c11d663aedab793477d7f942dba541c053ea0d93
noumecha spaker <noumechaivan@gmail.com>
2023-02-24 14:38:07 +0100

    maj : EventSubscriber with class fixing bugs

--- src/EventSubscriber/LearnModuleRedirectSubscriber.php
+++ src/EventSubscriber/LearnModuleRedirectSubscriber.php
@@ -1,66 +1,103 @@
-<?php 
+<?php
 namespace Drupal\learn_module\EventSubscriber;
 
 use Drupal\Core\Session\AccountProxyInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+#use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Routing\LocalRedirectResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Url;
 
 /***
- * Subscribes to the kernet Request evnet and redircets to the home page 
+ * Subscribes to the kernet Request evnet and redircets to the home page
  * When the user has the "non_grata" role.
- * 
+ *
  */
 
-class LearnModuleRedirectSubscriber implements EventSubscriberInterface 
+class LearnModuleRedirectSubscriber implements EventSubscriberInterface
 {
     /**
-     * 
+     *
      * @var \Drupal\Core\Session\AccountProxyInterface
      */
     protected $currentUser;
 
     /**
-     * 
+     *
+     * @var \Drupal\Core\Routing\CurrentRouteMatch
+     */
+    protected $currentRouteMatch;
+
+    /**
+     *
      * LearnModuleRedirectSubscriber constructor.
-     * 
+     *
      * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
      */
 
     public function __construct(AccountProxyInterface $currentUser)
     {
+        //$this->currentRouteMatch = $currentRouteMatch;
         $this->currentUser = $currentUser;
     }
 
     /**
      * {@inheritdoc}
+     * get the service via the create() method
      */
-    public static function getSubscribedEvents() 
+    public static function create(ContainerInterface $container) {
+        return new static(
+            $container->get('learn_module.redirect_subscribe')
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     * we use this function genrerally to subscribe to some event
+     */
+    public static function getSubscribedEvents()
     {
-        $events['kernel.request'][] = ['onRequet',0];
+        //$events['kernel.request'][] = ['onRequet',0];
+        $events[KernelEvents::REQUEST][] = ['onRequet',0];
         return $events;
     }
-    
+
     /**
      * Handler for the kernel request event.
-     * 
+     *
      * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
      */
     public function onRequet(GetResponseEvent $event)
     {
-        $request = $event->getRequest();
+
+        $route_name = $this->currentRouteMatch->getRouteName();
+        //dump($route_name);
+        if ($route_name !== 'learn_module.redirect_subscribe')
+        {
+            return ;
+        }
+        $roles = $this->currentUser->getRoles();
+        if(in_array('authenticated', $roles))
+        {
+            $url = Url::fromUri('internal:/redirect');
+            $event->setResponse(new LocalRedirectResponse($url->toString()));
+        }
+        /*$request = $event->getRequest();
         $path = $request->getPathInfo();
 
-        if($path !== '/redirect') 
+        if($path !== '/redirect')
         {
             return;
         }
 
         $roles = $this->currentUser->getRoles();
-        //dump($roles);
+        //dump($request);
         if(in_array('authenticated', $roles))
         {
             $event->setResponse(new RedirectResponse('/node/1'));
-        }
+        }*/
     }
 }
